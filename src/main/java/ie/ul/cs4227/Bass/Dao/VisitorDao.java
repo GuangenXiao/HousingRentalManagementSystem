8c262df998015bf259bc8973de6b27b9cf31dca7
GuangenXiao <19107773@studentmail.ul.ie>
2021-03-21 15:28:21 +0800

    visitor dao tag bugs

--- src/main/java/ie/ul/cs4227/Bass/Dao/VisitorDao.java
+++ src/main/java/ie/ul/cs4227/Bass/Dao/VisitorDao.java
@@ -2,8 +2,12 @@ package ie.ul.cs4227.Bass.Dao;
 
 import java.util.ArrayList;
 
-import ie.ul.cs4227.Bass.Entity.Visitor;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+import ie.ul.cs4227.Bass.Entity.Visitor;
+@Repository
+@Mapper
 public interface VisitorDao {
  public abstract Integer insertVisitor(Visitor v);
  public abstract ArrayList<Visitor> getVisitors();
