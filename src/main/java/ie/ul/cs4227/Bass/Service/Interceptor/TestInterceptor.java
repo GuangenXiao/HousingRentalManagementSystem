08fe519a84bf250f733470244b5919e864530ca3
GuangenXiao <19107773@studentmail.ul.ie>
2021-03-28 16:32:25 +0800

    rename interceptor

--- src/main/java/ie/ul/cs4227/Bass/Service/Interceptor/TestInterceptor.java
+++ src/main/java/ie/ul/cs4227/Bass/Service/Interceptor/TestInterceptor.java
@@ -1,6 +1,6 @@
-package ie.ul.cs4227.Bass.Service.Intercepter;
+package ie.ul.cs4227.Bass.Service.Interceptor;
 
-public class TestIntercepter {
+public class TestInterceptor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
